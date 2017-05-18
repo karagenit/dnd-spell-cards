70e436170c6e5b7f1607cad2b21bbf888825693e
Caleb Smith <karagenit@outlook.com>
2017-05-17 20:30:46 -0400

    Use High-Resolution Template

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -15,9 +16,23 @@ import org.apache.commons.csv.CSVRecord;
 
 public class Main {
 	
+	/*
+	 * Normal MtG card is 2.5 x 3.5 inches
+	 * Which, at a "normal printing of 300 ppi, gives us a required resolution of
+	 * 750x1050 px
+	 * 
+	 * For printing:
+	 * portrait could print 3x3 = 9
+	 * landscape could print 4x2 = 8
+	 * 
+	 * System.out.println(Arrays.toString(Font.getFamilies().toArray()));
+	 * 
+	 */
+	
 	public static final String csvFileName = "./res/csv/sample.csv";
-	public static final String templateFileName = "./res/images/template.jpg";
+	public static final String templateFileName = "./res/images/template-2.jpg";
 	public static final String abjurationFileName = "./res/images/abjuration.jpg";
+	public static final Font font = new Font("Times New Roman", Font.BOLD, 32);
 
 	public enum SpellType {
 		Abjuration
@@ -31,14 +46,15 @@ public class Main {
 		for(CSVRecord record : parser) {
 			BufferedImage img = ImageIO.read(new File(templateFileName));
 			
+			//img.getr //TODO background color change
+			
 			Graphics2D g = img.createGraphics();
 			
 			writeTop(g, record.get("Spell Name"));
 			writeDesc(g, record.get("Spell Description"));
-			writeImg(g, SpellType.Abjuration);
-
+			writeImg(g, SpellType.Abjuration);			
 			
-			ImageIO.write(img, "png", new File("./res/images/sample-" + cardCount + ".png"));
+			ImageIO.write(img, "png", new File("./res/images/test-" + cardCount + ".png"));
 			
 			cardCount++;
 		}	
@@ -59,27 +75,31 @@ public class Main {
 			break;
 		}
 		
-		g.drawImage(img, 21, 40, 215-21, 182-40, getEmptyObserver()); //TODO yuck it's not square
+		g.drawImage(img, 64, 125, 685-64, 580-125, getEmptyObserver()); //TODO make non-stretching
 	}
 
 	public static void writeTop(Graphics2D g, String s) {
 		g.setColor(Color.BLACK);
-		g.drawString(s, 22, 31);
+		g.setFont(font);
+		g.drawString(s, 70, 95); 
 	}
 	
 	public static void writeMiddle(Graphics2D g, String s) {
 		g.setColor(Color.BLACK);
-		g.drawString(s, 22, 199);
+		g.setFont(font);
+		g.drawString(s, 70, 630);
 	}
 	
-	public static void writeDesc(Graphics2D g, String s) {
+	public static void writeDesc(Graphics2D g, String s) { //TODO multiple lines
 		g.setColor(Color.BLACK);
-		g.drawString(s, 24, 220);
+		g.setFont(font);
+		g.drawString(s, 75, 690);
 	}
 	
 	public static void writeBottom(Graphics2D g, String s) {
 		g.setColor(Color.BLACK);
-		g.drawString(s, 181, 307);
+		g.setFont(font);
+		g.drawString(s, 575, 975);
 	}
 	
 	public static ImageObserver getEmptyObserver() {
